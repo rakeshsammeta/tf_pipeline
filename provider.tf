90351958cae4b36f1a749b63281b1e7e310bfd0f
rakesh <rakeshsammeta@gmail.com>
2020-09-25 16:46:11 +0530

    added files

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  version         = "=2.4.0"
+  subscription_id = "5352c37d-5784-417d-a370-3a94ec131218"
+  client_id       = "435341a6-cb2d-468e-aadd-00530b3581dc"
+  client_secret   = "Ao_4-tOAlB_UKPEw~j5rU_k6r05PPM6Bq8"
+  tenant_id       = "89c31420-fd84-4f93-a145-9f51ec990bfb"
+  features {}
+}
